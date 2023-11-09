2c168a41fb68d62577f429ad6acdf15471e52508
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-11-09 17:01:53 +0100

    feat(models): Agregamos validaciones a client

--- src/main/java/com/nullers/restbookstore/client/models/Client.java
+++ src/main/java/com/nullers/restbookstore/client/models/Client.java
@@ -1,10 +1,12 @@
 package com.nullers.restbookstore.client.models;
 
+import com.nullers.restbookstore.NOADD.models.Book;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,15 +22,29 @@ public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(nullable = false)
     private String name;
+
+    @Column(nullable = false)
     private String surname;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String phone;
+
+    @Column(nullable = false)
     private String address;
 
     @OneToMany
     @JoinColumn(name = "book_id")
     private List<Book> books;
+
+    @CreatedDate
     private LocalDateTime createdAt;
-    private String qrImage;
+
+
+
 }
