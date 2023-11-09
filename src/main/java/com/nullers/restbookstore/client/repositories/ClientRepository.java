6af39cc45a2fee6063e2bf3b08cfec4691eb135d
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-11-09 17:02:26 +0100

    feat(repository): Creamos repositorio de clientes

--- src/main/java/com/nullers/restbookstore/client/repositories/ClientRepository.java
+++ src/main/java/com/nullers/restbookstore/client/repositories/ClientRepository.java
@@ -4,10 +4,13 @@ import com.nullers.restbookstore.client.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, UUID> {
+
+    public Optional<Client> getClientByEmail(String email);
 
 
 }
