e39d005b6bcba0ee924d7f9d1dc10f18a7e62183
Binweiwang <binweiwang1029@gmail.com>
2023-11-09 19:04:51 +0100

    feat(dto): DTO UserResponse, UserRequest y UserInfoResponse agregados

---
+++ src/main/java/com/nullers/restbookstore/user/dto/UserResponse.java
@@ -0,0 +1,26 @@
+package com.nullers.restbookstore.user.dto;
+
+import com.nullers.restbookstore.user.model.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponse {
+    private UUID id;
+    private String nombre;
+    private String apellidos;
+    private String username;
+    private String email;
+    @Builder.Default
+    private Set<Role> roles = Set.of(Role.USER);
+    private Boolean isDeleted = false;
+
+}
