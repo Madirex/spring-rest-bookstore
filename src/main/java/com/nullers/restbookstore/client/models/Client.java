994e922ed79584748a1a2d3133a1b55ecbc75a93
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-11-12 13:24:29 +0100

    feat(model): Añadimos imagen por defecto y lista de libros
    Añadimos javadoc

--- src/main/java/com/nullers/restbookstore/client/models/Client.java
+++ src/main/java/com/nullers/restbookstore/client/models/Client.java
@@ -2,23 +2,29 @@ package com.nullers.restbookstore.client.models;
 
 import com.nullers.restbookstore.NOADD.models.Book;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+/**
+ * @author daniel
+ * @see Book
+ * Modelo de cliente
+ */
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "clients")
+@ToString
 public class Client {
+    public static final String DEFAULT_IMAGE = "https://via.placeholder.com/150";
+
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
@@ -35,16 +41,19 @@ public class Client {
     @Column(nullable = false)
     private String phone;
 
+    @Column(columnDefinition = "TEXT default '" + DEFAULT_IMAGE + "'") // Por defecto una imagen
+    @Builder.Default
+    private String image = DEFAULT_IMAGE;
+
     @Column(nullable = false)
     private String address;
 
     @OneToMany
     @JoinColumn(name = "book_id")
-    private List<Book> books;
+    @Builder.Default
+    private List<Book> books = new ArrayList<>();
 
     @CreatedDate
     private LocalDateTime createdAt;
-
-
 
 }
