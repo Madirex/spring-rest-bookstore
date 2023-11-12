30f4e72d40b3f06e07da32f6483e068f871c901a
Binweiwang <binweiwang1029@gmail.com>
2023-11-13 00:08:55 +0100

    feat(exception): exceptiones jdoc agregados

--- src/main/java/com/nullers/restbookstore/user/exception/UserNameOrEmailExists.java
+++ src/main/java/com/nullers/restbookstore/user/exception/UserNameOrEmailExists.java
@@ -3,6 +3,9 @@ package com.nullers.restbookstore.user.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Excepcion de usuario no encontrado
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNameOrEmailExists extends UserException {
     public UserNameOrEmailExists(String message) {
--- src/main/java/com/nullers/restbookstore/user/exception/UserNotFound.java
+++ src/main/java/com/nullers/restbookstore/user/exception/UserNotFound.java
@@ -3,11 +3,15 @@ package com.nullers.restbookstore.user.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Excepcion de usuario no encontrado
+ */
 @ResponseStatus(HttpStatus.NOT_FOUND)
-public class UserNotFound extends UserException{
+public class UserNotFound extends UserException {
     public UserNotFound(String message) {
         super(message);
     }
+
     public UserNotFound(Long id) {
         super("Usuario con id" + id + "no encontrado");
     }
