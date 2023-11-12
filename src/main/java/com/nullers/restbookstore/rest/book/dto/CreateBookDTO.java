197e0b3369324c7a916acdff89068d56257514b1
Madirex <madirexland@gmail.com>
2023-11-12 22:36:02 +0100

    feat(dto): Agregado campo de price

--- src/main/java/com/nullers/restbookstore/rest/book/dto/CreateBookDTO.java
+++ src/main/java/com/nullers/restbookstore/rest/book/dto/CreateBookDTO.java
@@ -1,5 +1,6 @@
 package com.nullers.restbookstore.rest.book.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
@@ -16,8 +17,12 @@ public class CreateBookDTO {
     @NotBlank(message = "El nombre no puede estar vacío")
     private String name;
 
-    @NotNull(message = "publisher no puede ser nulo")
+    @NotNull(message = "Publisher no puede ser nulo")
     private String publisherId;
+
+    @Min(value = 0, message = "El precio no puede estar en negativo")
+    @NotNull(message = "El precio no puede ser nulo")
+    private Double price;
 
     @NotBlank(message = "La imagen no puede estar vacía")
     private String image;
