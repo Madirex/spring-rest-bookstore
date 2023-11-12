4acae0e16a7a1059bb9234f19ccaf42be078a317
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-11-12 13:23:29 +0100

    feat(repository): Añadimos pageable

--- src/main/java/com/nullers/restbookstore/client/repositories/ClientRepository.java
+++ src/main/java/com/nullers/restbookstore/client/repositories/ClientRepository.java
@@ -2,13 +2,14 @@ package com.nullers.restbookstore.client.repositories;
 
 import com.nullers.restbookstore.client.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface ClientRepository extends JpaRepository<Client, UUID> {
+public interface ClientRepository extends JpaRepository<Client, UUID>, JpaSpecificationExecutor<Client> {
 
     public Optional<Client> getClientByEmail(String email);
 
