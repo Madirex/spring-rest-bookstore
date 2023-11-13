252b0645edc27074d2002b343aaa614e3427deae
Madirex <madirexland@gmail.com>
2023-11-13 12:13:29 +0100

    docs(mappers): Mappers documentados con JDOC

--- src/main/java/com/nullers/restbookstore/rest/book/mappers/BookMapper.java
+++ src/main/java/com/nullers/restbookstore/rest/book/mappers/BookMapper.java
@@ -10,13 +10,42 @@ import java.util.List;
 
 /**
  * Clase BookMapper
+ *
+ * @Author Madirex
  */
 public interface BookMapper {
+    /**
+     * Mapea un CreateBookDTO en Book
+     *
+     * @param dto       CreateBookDTO a mapear
+     * @param publisher Publisher a mapear
+     * @return Book mapeado
+     */
     Book toBook(CreateBookDTO dto, Publisher publisher);
 
+    /**
+     * Mapea un UpdateBookDTO en Book
+     *
+     * @param book      Book a mapear
+     * @param dto       UpdateBookDTO a mapear
+     * @param publisher Publisher a mapear
+     * @return Book mapeado
+     */
     Book toBook(Book book, UpdateBookDTO dto, Publisher publisher);
 
+    /**
+     * Mapea un Book en GetBookDTO
+     *
+     * @param book Book a mapear
+     * @return GetBookDTO mapeado
+     */
     GetBookDTO toGetBookDTO(Book book);
 
+    /**
+     * Mapea una lista de Book en una lista de GetBookDTO
+     *
+     * @param dto Book a mapear
+     * @return GetBookDTO mapeado
+     */
     List<GetBookDTO> toBookList(List<Book> dto);
 }
