b3f8179d545dde57352778a01571eec3cb31f129
Madirex <madirexland@gmail.com>
2023-11-13 11:52:57 +0100

    fix(exceptions): Cambiado UUID por ID

--- src/main/java/com/nullers/restbookstore/rest/book/exceptions/BookNotValidIDException.java
+++ src/main/java/com/nullers/restbookstore/rest/book/exceptions/BookNotValidIDException.java
@@ -4,18 +4,18 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 /**
- * Class BookNotValidUUIDException
+ * Class BookNotValidIDException
  *
  * @Author Madirex
  */
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class BookNotValidUUIDException extends BookException {
+public class BookNotValidIDException extends BookException {
     /**
-     * Constructor BookNotValidUUIDException
+     * Constructor BookNotValidIDException
      *
      * @param message Mensaje de error
      */
-    public BookNotValidUUIDException(String message) {
-        super("UUID no válido - " + message);
+    public BookNotValidIDException(String message) {
+        super("ID no válido - " + message);
     }
 }
