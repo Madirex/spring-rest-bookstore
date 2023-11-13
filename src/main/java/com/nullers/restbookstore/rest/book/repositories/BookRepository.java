4977fb9be06adc439267ca5f647bf9c0b4f54389
Madirex <madirexland@gmail.com>
2023-11-13 11:52:26 +0100

    fix(repositories): Cambiado UUID por ID

--- src/main/java/com/nullers/restbookstore/rest/book/repositories/BookRepository.java
+++ src/main/java/com/nullers/restbookstore/rest/book/repositories/BookRepository.java
@@ -5,12 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.UUID;
-
 /**
  * Interface BookRepository
  */
 @Repository
-public interface BookRepository extends JpaRepository<Book, UUID>, JpaSpecificationExecutor<Book> {
+public interface BookRepository extends JpaRepository<Book, Long>, JpaSpecificationExecutor<Book> {
 
 }
