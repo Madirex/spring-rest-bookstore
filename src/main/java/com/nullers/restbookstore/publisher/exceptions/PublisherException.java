28f9d064673e289833af3e5928e18d6b522c0d73
jaimesalcedo1 <salcedojaime58@gmail.com>
2023-11-13 18:41:36 +0100

    docs(exceptions): excepciones documentadas con JDOC

--- src/main/java/com/nullers/restbookstore/publisher/exceptions/PublisherException.java
+++ src/main/java/com/nullers/restbookstore/publisher/exceptions/PublisherException.java
@@ -1,5 +1,10 @@
 package com.nullers.restbookstore.publisher.exceptions;
 
+/**
+ * Clase PublisherException
+ *
+ * @author jaimesalcedo1
+ * */
 public abstract class PublisherException extends RuntimeException{
     protected PublisherException(String message){
         super(message);
--- src/main/java/com/nullers/restbookstore/publisher/exceptions/PublisherUUIDNotValid.java
+++ src/main/java/com/nullers/restbookstore/publisher/exceptions/PublisherUUIDNotValid.java
@@ -3,6 +3,11 @@ package com.nullers.restbookstore.publisher.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Clase PublisherUUIDNotValid
+ *
+ * @author jaimesalcedo1
+ * */
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class PublisherUUIDNotValid extends PublisherException{
 
