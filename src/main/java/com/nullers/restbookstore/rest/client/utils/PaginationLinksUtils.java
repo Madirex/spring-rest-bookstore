d298cd660cf5b6c9438fe963a364b79daf47afc6
daniel <daniell.gm03@gmail.com>
2023-11-15 16:00:55 +0100

    style(utils): Cambiamos imports a rest

--- src/main/java/com/nullers/restbookstore/rest/client/utils/PaginationLinksUtils.java
+++ src/main/java/com/nullers/restbookstore/rest/client/utils/PaginationLinksUtils.java
@@ -1,4 +1,4 @@
-package com.nullers.restbookstore.client.utils;
+package com.nullers.restbookstore.rest.client.utils;
 
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
