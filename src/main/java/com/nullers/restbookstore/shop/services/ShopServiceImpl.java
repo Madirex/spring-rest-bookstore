1d31e8c5ad87d4e3618b3f4b3a506179d375ae72
Alexandru <alexandru.dorobat@educa.madrid.org>
2023-11-15 19:36:26 +0100

    docs(services):servicios Shop documentados con JPA

--- src/main/java/com/nullers/restbookstore/shop/services/ShopServiceImpl.java
+++ src/main/java/com/nullers/restbookstore/shop/services/ShopServiceImpl.java
@@ -15,18 +15,33 @@ import java.util.List;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
+/**
+ * Servicio que implementa las operaciones de negocio para la entidad Shop.
+ * Utiliza ShopRepository para acceso a datos y ShopMapper para la conversión entre entidades y DTOs.
+ *
+ * @author alexdor00
+ */
 @Service
 public class ShopServiceImpl implements ShopService {
 
     private final ShopRepository shopRepository;
     private final ShopMapperImpl shopMapper;
 
+    /**
+     * Constructor que inyecta el repositorio de tiendas y el mapper.
+     * @param shopRepository Repositorio para las operaciones de base de datos de Shop.
+     * @param shopMapper Mapper para convertir entre Shop y sus DTOs.
+     */
     @Autowired
     public ShopServiceImpl(ShopRepository shopRepository, ShopMapperImpl shopMapper) {
         this.shopRepository = shopRepository;
         this.shopMapper = shopMapper;
     }
 
+    /**
+     * Obtiene todas las tiendas y las convierte a DTOs.
+     * @return Lista de tiendas en forma de DTOs.
+     */
     @Override
     public List<GetShopDto> getAllShops() {
         return shopRepository.findAll().stream()
@@ -34,6 +49,13 @@ public class ShopServiceImpl implements ShopService {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Obtiene una tienda por su UUID y la convierte a DTO.
+     * @param id Identificador UUID de la tienda.
+     * @return DTO de la tienda encontrada.
+     * @throws ShopNotValidUUIDException Si el UUID proporcionado no es válido.
+     * @throws ShopNotFoundException Si la tienda no se encuentra.
+     */
     @Override
     public GetShopDto getShopById(String id) throws ShopNotValidUUIDException, ShopNotFoundException {
         UUID uuid;
@@ -48,6 +70,11 @@ public class ShopServiceImpl implements ShopService {
         return shopMapper.toGetShopDto(shop);
     }
 
+    /**
+     * Crea una nueva tienda a partir de un DTO y la guarda en la base de datos.
+     * @param shopDto DTO con los datos para crear la tienda.
+     * @return DTO de la tienda creada.
+     */
     @Override
     public GetShopDto createShop(CreateShopDto shopDto) {
         Shop shop = shopMapper.toShop(shopDto);
@@ -55,6 +82,14 @@ public class ShopServiceImpl implements ShopService {
         return shopMapper.toGetShopDto(shop);
     }
 
+    /**
+     * Actualiza una tienda existente con los datos proporcionados en el DTO.
+     * @param id Identificador UUID de la tienda a actualizar.
+     * @param shopDto DTO con los datos actualizados.
+     * @return DTO de la tienda actualizada.
+     * @throws ShopNotValidUUIDException Si el UUID proporcionado no es válido.
+     * @throws ShopNotFoundException Si la tienda no se encuentra.
+     */
     @Override
     public GetShopDto updateShop(String id, UpdateShopDto shopDto) throws ShopNotValidUUIDException, ShopNotFoundException {
         UUID uuid;
@@ -71,6 +106,12 @@ public class ShopServiceImpl implements ShopService {
         return shopMapper.toGetShopDto(shop);
     }
 
+    /**
+     * Elimina una tienda de la base de datos por su UUID.
+     * @param id Identificador UUID de la tienda a eliminar.
+     * @throws ShopNotFoundException Si la tienda no se encuentra.
+     * @throws ShopNotValidUUIDException Si el UUID proporcionado no es válido.
+     */
     @Override
     public void deleteShop(String id) throws ShopNotFoundException, ShopNotValidUUIDException {
         UUID uuid;
