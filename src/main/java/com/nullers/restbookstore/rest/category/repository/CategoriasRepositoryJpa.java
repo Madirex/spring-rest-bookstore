47a610b5da67c67439eaf96d57937bedca9757f0
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-11-15 22:36:08 +0100

    feat(repository): Creamos repositorio de categoria

---
+++ src/main/java/com/nullers/restbookstore/rest/category/repository/CategoriasRepositoryJpa.java
@@ -0,0 +1,17 @@
+package com.nullers.restbookstore.rest.category.repository;
+
+import com.nullers.restbookstore.rest.category.models.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CategoriasRepositoryJpa extends JpaRepository<Categoria, UUID>, JpaSpecificationExecutor<Categoria> {
+
+    public Optional<Categoria> findById(UUID id);
+
+    public Optional<Categoria> findByNombre(String nombre);
+}
