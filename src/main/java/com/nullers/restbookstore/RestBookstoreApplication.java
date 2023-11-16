081299bfaf6a6341e5c9b1db30668a68b50fb5df
Madirex <madirexland@gmail.com>
2023-11-16 01:23:46 +0100

    fix(RestBookstoreApplication): Solucionados problemas Caché

--- src/main/java/com/nullers/restbookstore/RestBookstoreApplication.java
+++ src/main/java/com/nullers/restbookstore/RestBookstoreApplication.java
@@ -2,13 +2,11 @@ package com.nullers.restbookstore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 /**
  * Clase RestBookstoreApplication
  */
 @SpringBootApplication
-@EnableCaching
 public class RestBookstoreApplication {
 
     /**
