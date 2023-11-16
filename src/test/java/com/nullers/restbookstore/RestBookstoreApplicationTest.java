c43505d2f81c4ea042a1407a26e06c878d8e472a
Madirex <madirexland@gmail.com>
2023-11-16 19:26:13 +0100

    test(restBookstoreApplication): Agregado test MAIN

---
+++ src/test/java/com/nullers/restbookstore/RestBookstoreApplicationTest.java
@@ -0,0 +1,15 @@
+package com.nullers.restbookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest(useMainMethod = SpringBootTest.UseMainMethod.ALWAYS)
+class RestBookstoreApplicationTest {
+
+    @Test
+    void testMain() {
+        assertThrows(IllegalArgumentException.class, () -> RestBookstoreApplication.main(null));
+    }
+}
