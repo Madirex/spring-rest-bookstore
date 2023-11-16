291bf30c56bb0a33380e22ec3c5349dbbd2a56c7
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-11-16 02:11:41 +0100

    feat(exceptions): Creamos excepcion

---
+++ src/main/java/com/nullers/restbookstore/rest/client/exceptions/ClientBookAlreadyExists.java
@@ -0,0 +1,8 @@
+package com.nullers.restbookstore.rest.client.exceptions;
+
+public class ClientBookAlreadyExists extends ClientException{
+
+    public ClientBookAlreadyExists(String message) {
+        super(message);
+    }
+}
