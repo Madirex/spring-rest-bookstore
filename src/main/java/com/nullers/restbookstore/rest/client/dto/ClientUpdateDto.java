470ef354593a9703c8abcaf0ddbc232b6e333a90
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-11-16 01:14:48 +0100

    fix(dto): Arreglamos dto al mergear

--- src/main/java/com/nullers/restbookstore/rest/client/dto/ClientUpdateDto.java
+++ src/main/java/com/nullers/restbookstore/rest/client/dto/ClientUpdateDto.java
@@ -1,7 +1,6 @@
 package com.nullers.restbookstore.rest.client.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
