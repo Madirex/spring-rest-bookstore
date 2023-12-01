2ebf0102285658915f6634743bc11f39f543b462
Binweiwang <binweiwang1029@gmail.com>
2023-12-01 17:48:09 +0100

    feat(authusersservices): Agregados AuthUsersServices

---
+++ src/main/java/com/nullers/restbookstore/rest/auth/services/users/AuthUsersServiceImp.java
@@ -0,0 +1,40 @@
+package com.nullers.restbookstore.rest.auth.services.users;
+
+import com.nullers.restbookstore.rest.auth.repositories.AuthUsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+/**
+ * AuthUsersServiceImp implements AuthUsersService
+ *
+ * @Author: Binwei Wang
+ */
+@Service("userDetailsService")
+public class AuthUsersServiceImp implements AuthUsersService {
+    /**
+     * AuthUsersRepository
+     */
+    private final AuthUsersRepository authUsersRepository;
+
+    /**
+     * Constructor for AuthUsersServiceImp
+     * @param authUsersRepository AuthUsersRepository
+     */
+
+    @Autowired
+    public AuthUsersServiceImp(AuthUsersRepository authUsersRepository) {
+        this.authUsersRepository = authUsersRepository;
+    }
+
+    /**
+     * loadUserByUsername
+     * @param username Nombre de usuario
+     * @return UserDetails
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        return authUsersRepository.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("Usuario con username " + username + " no encontrado"));
+    }
+}
