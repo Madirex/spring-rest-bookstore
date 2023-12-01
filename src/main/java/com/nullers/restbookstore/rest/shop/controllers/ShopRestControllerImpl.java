925938927ce8f19df4578a8666717c462f06b401
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-12-02 00:22:51 +0100

    feat(controllers): Agregamos excepciones handler y nuevas peticiones
    peticiones para libros y clientes

--- src/main/java/com/nullers/restbookstore/rest/shop/controllers/ShopRestControllerImpl.java
+++ src/main/java/com/nullers/restbookstore/rest/shop/controllers/ShopRestControllerImpl.java
@@ -1,25 +1,34 @@
 package com.nullers.restbookstore.rest.shop.controllers;
 
 import com.nullers.restbookstore.pagination.exceptions.PageNotValidException;
+import com.nullers.restbookstore.pagination.models.ErrorResponse;
 import com.nullers.restbookstore.pagination.models.PageResponse;
 import com.nullers.restbookstore.pagination.util.PaginationLinksUtils;
+import com.nullers.restbookstore.rest.book.exceptions.BookNotFoundException;
+import com.nullers.restbookstore.rest.client.exceptions.ClientNotFound;
+import com.nullers.restbookstore.rest.common.PageableRequest;
 import com.nullers.restbookstore.rest.shop.dto.CreateShopDto;
 import com.nullers.restbookstore.rest.shop.dto.GetShopDto;
 import com.nullers.restbookstore.rest.shop.dto.UpdateShopDto;
+import com.nullers.restbookstore.rest.shop.exceptions.ShopHasOrders;
 import com.nullers.restbookstore.rest.shop.exceptions.ShopNotFoundException;
 import com.nullers.restbookstore.rest.shop.exceptions.ShopNotValidUUIDException;
 import com.nullers.restbookstore.rest.shop.services.ShopServiceImpl;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
+import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Repository;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.util.Optional;
+import java.util.*;
 
 /**
  * Implementación del controlador REST para la gestión de tiendas (Shops).
@@ -52,17 +61,16 @@ public class ShopRestControllerImpl implements ShopRestController {
      * @return ResponseEntity con una lista de todas las tiendas en formato DTO.
      */
     @GetMapping
-    public ResponseEntity<PageResponse<GetShopDto>> getAllShops(@Valid @RequestParam(required = false) Optional<String> name,
-                                                                @RequestParam(required = false) Optional<String> location,
-                                                                @RequestParam(defaultValue = "0") int page,
-                                                                @RequestParam(defaultValue = "10") int size,
-                                                                @RequestParam(defaultValue = "id") String sortBy,
-                                                                @RequestParam(defaultValue = "asc") String direction,
-                                                                HttpServletRequest request
+    public ResponseEntity<PageResponse<GetShopDto>> getAllShops(
+            @Valid @RequestParam(required = false) Optional<String> name,
+            @RequestParam(required = false) Optional<String> location,
+            @Valid PageableRequest pageableRequest,
+            HttpServletRequest request
     ) {
-        if (page < 0 || size < 1) {
-            throw new PageNotValidException("La página no puede ser menor que 0 y su tamaño no debe de ser menor a 1.");
-        }
+        int page = pageableRequest.getPage();
+        int size = pageableRequest.getSize();
+        String sortBy = pageableRequest.getOrderBy();
+        String direction = pageableRequest.getOrder();
         Sort sort = direction.equalsIgnoreCase(
                 Sort.Direction.ASC.name()) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
         UriComponentsBuilder uriBuilder = UriComponentsBuilder.fromHttpUrl(request.getRequestURL().toString());
@@ -81,12 +89,8 @@ public class ShopRestControllerImpl implements ShopRestController {
      */
     @GetMapping("/{id}")
     @Override
-    public ResponseEntity<GetShopDto> getShopById(@Valid @PathVariable String id) throws ShopNotFoundException {
-        try {
-            return ResponseEntity.ok(shopService.getShopById(id));
-        } catch (ShopNotValidUUIDException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null); // UUID no válido
-        }
+    public ResponseEntity<GetShopDto> getShopById(@Valid @PathVariable UUID id) throws ShopNotFoundException {
+        return ResponseEntity.ok(shopService.getShopById(id));
     }
 
     /**
@@ -112,13 +116,8 @@ public class ShopRestControllerImpl implements ShopRestController {
      */
     @PutMapping("/{id}")
     @Override
-    public ResponseEntity<GetShopDto> updateShop(@Valid @PathVariable String id, @Valid @RequestBody UpdateShopDto shopDto)
-            throws ShopNotFoundException {
-        try {
-            return ResponseEntity.ok(shopService.updateShop(id, shopDto));
-        } catch (ShopNotValidUUIDException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null); // UUID no válido
-        }
+    public ResponseEntity<GetShopDto> updateShop(@Valid @PathVariable UUID id, @Valid @RequestBody UpdateShopDto shopDto) {
+        return ResponseEntity.ok(shopService.updateShop(id, shopDto));
     }
 
     /**
@@ -130,12 +129,81 @@ public class ShopRestControllerImpl implements ShopRestController {
      */
     @DeleteMapping("/{id}")
     @Override
-    public ResponseEntity<String> deleteShop(@Valid @PathVariable String id) throws ShopNotFoundException {
-        try {
-            shopService.deleteShop(id);
-            return ResponseEntity.noContent().build();
-        } catch (ShopNotValidUUIDException e) {
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build(); // UUID no válido
-        }
+    public ResponseEntity<String> deleteShop(@PathVariable UUID id) {
+        shopService.deleteShop(id);
+        return ResponseEntity.noContent().build();
+    }
+
+
+    @PatchMapping("/{id}/books/{bookId}")
+    public ResponseEntity<GetShopDto> addBookToShop(@Valid @PathVariable UUID id, @Valid @PathVariable Long bookId) {
+        return ResponseEntity.ok(shopService.addBookToShop(id, bookId));
+    }
+
+    @Override
+    @DeleteMapping("/{id}/books/{bookId}")
+    public ResponseEntity<GetShopDto> removeBookFromShop(UUID id, Long bookId) {
+        return ResponseEntity.ok(shopService.removeBookFromShop(id, bookId));
+    }
+
+    @Override
+    @PatchMapping("/{id}/clients/{clientId}")
+    public ResponseEntity<GetShopDto> addClientToShop(UUID id, UUID clientId) {
+        return ResponseEntity.ok(shopService.addClientToShop(id, clientId));
+    }
+
+    @Override
+    @DeleteMapping("/{id}/clients/{clientId}")
+    public ResponseEntity<GetShopDto> removeClientFromShop(UUID id, UUID clientId) {
+        return ResponseEntity.ok(shopService.removeClientFromShop(id, clientId));
+    }
+
+
+    @ExceptionHandler(ClientNotFound.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorResponse> handleClientNotFound(ClientNotFound ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage()));
+    }
+
+    @ExceptionHandler(ShopNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorResponse> handleShopNotFound(ShopNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage()));
+    }
+
+    @ExceptionHandler(BookNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ResponseEntity<ErrorResponse> handleBookNotFound(BookNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new ErrorResponse(HttpStatus.NOT_FOUND.value(), ex.getMessage()));
+    }
+
+    @ExceptionHandler(ShopHasOrders.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ErrorResponse> handleShopHasOrders(ShopHasOrders ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage()));
+    }
+
+    /**
+     * Manejador de excepciones de MethodArgumentNotValidException
+     *
+     * @param ex excepción
+     * @return ResponseEntity<Map < String, Object>> con el mensaje de error
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        Map<String, Object> response = new LinkedHashMap<>();
+        response.put("code", HttpStatus.BAD_REQUEST.value());
+        response.put("errors", errors);
+
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
     }
 }
