ef94027182eba4cd3580f33f267b557bbef5e6db
Binweiwang <binweiwang1029@gmail.com>
2023-12-02 20:05:50 +0100

    feat(userinforesponse): Agregado order

--- src/main/java/com/nullers/restbookstore/rest/user/dto/UserInfoResponse.java
+++ src/main/java/com/nullers/restbookstore/rest/user/dto/UserInfoResponse.java
@@ -1,8 +1,11 @@
 package com.nullers.restbookstore.rest.user.dto;
 
+import com.nullers.restbookstore.rest.orders.models.Order;
 import com.nullers.restbookstore.rest.user.models.Role;
 import lombok.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
@@ -20,6 +23,8 @@ public class UserInfoResponse {
     private String surnames;
     private String username;
     private String email;
+    @Builder.Default
+    private List<String> order = new ArrayList<>();
     @Builder.Default
     private Set<Role> roles = Set.of(Role.USER);
     @Builder.Default
