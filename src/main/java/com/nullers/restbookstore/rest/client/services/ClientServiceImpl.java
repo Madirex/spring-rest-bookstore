62975bcef933fe613dfa99ff33fa20c66aa5a0c7
Daniel Garrido Muros <daniell.gm03@gmail.com>
2023-12-02 14:22:52 +0100

    fix(services): Quitamos todo de books de cliente

--- src/main/java/com/nullers/restbookstore/rest/client/services/ClientServiceImpl.java
+++ src/main/java/com/nullers/restbookstore/rest/client/services/ClientServiceImpl.java
@@ -57,10 +57,7 @@ public class ClientServiceImpl implements ClientService{
 
     private final List<String> contentTypesAllowed = List.of("png", "jpeg", "jpg");
 
-
     private final ClientRepository clientRepository;
-
-    private final BookServiceImpl bookService;
     private final OrderRepository orderRepository;
 
     private final StorageService storageService;
@@ -71,20 +68,17 @@ public class ClientServiceImpl implements ClientService{
 
     private final ObjectMapper mapper;
 
-    private final BookMapperImpl bookMapper;
 
 
     @Autowired
     public ClientServiceImpl(ClientRepository clientRepository, BookServiceImpl bookService, OrderRepository orderRepository, StorageService storageService, WebSocketConfig webSocketConfig, ClientNotificationMapper clientNotificationMapper, BookMapperImpl bookMapper) {
         this.clientRepository = clientRepository;
-        this.bookService = bookService;
         this.orderRepository = orderRepository;
         this.storageService = storageService;
         this.webSocketConfig = webSocketConfig;
         this.clientNotificationMapper = clientNotificationMapper;
         webSocketService = webSocketConfig.webSocketClientsHandler();
         mapper = new ObjectMapper();
-        this.bookMapper = bookMapper;
         mapper.registerModule(new JavaTimeModule());
     }
 
