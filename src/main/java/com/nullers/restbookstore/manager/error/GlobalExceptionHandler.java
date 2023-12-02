a13548fa25ad1bae0fd049d1777a2af6f709cbdd
Madirex <madirexland@gmail.com>
2023-12-02 14:47:00 +0100

    feat(manager): manejador de errores MethodArgumentNotValidException

--- src/main/java/com/nullers/restbookstore/manager/error/GlobalExceptionHandler.java
+++ src/main/java/com/nullers/restbookstore/manager/error/GlobalExceptionHandler.java
@@ -1,23 +1,52 @@
 package com.nullers.restbookstore.manager.error;
 
 import com.nullers.restbookstore.pagination.exceptions.PageNotValidException;
-import com.nullers.restbookstore.rest.category.exceptions.CategoriaNotFoundException;
 import com.nullers.restbookstore.pagination.models.ErrorResponse;
+import com.nullers.restbookstore.rest.category.exceptions.CategoriaNotFoundException;
 import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
 import org.springframework.data.mapping.PropertyReferenceException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotWritableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Manejador de excepciones globales
  */
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    /**
+     * Método para manejar las excepciones de validación
+     *
+     * @param ex Excepción
+     * @return Mapa con los errores
+     */
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, Object>> handleValidationExceptions(
+            MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        Map<String, Object> response = new LinkedHashMap<>();
+        response.put("code", HttpStatus.BAD_REQUEST.value());
+        response.put("errors", errors);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+    }
 
     @ExceptionHandler(CategoriaNotFoundException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
