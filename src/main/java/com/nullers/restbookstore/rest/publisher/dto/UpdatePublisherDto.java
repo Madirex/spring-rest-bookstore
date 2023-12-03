2ee3ebada285996d912816208cc73f1a1b42b750
Jaime Salcedo Vallejo <salcedojaime58@gmail.com>
2023-12-03 21:11:05 +0100

    doc(publisher): añadida documentación swagger a modelo, dto, controlador

--- src/main/java/com/nullers/restbookstore/rest/publisher/dto/UpdatePublisherDto.java
+++ src/main/java/com/nullers/restbookstore/rest/publisher/dto/UpdatePublisherDto.java
@@ -1,5 +1,6 @@
 package com.nullers.restbookstore.rest.publisher.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 /**
@@ -9,7 +10,9 @@ import jakarta.validation.constraints.NotBlank;
  * */
 public class UpdatePublisherDto {
     @NotBlank(message = "el nombre no puede estar vacío")
+    @Schema(name = "Nombre", example = "Planeta")
     private String name;
     @NotBlank(message = "la imagen no puede estar vacía")
+    @Schema(name = "Imagen", example = "https://proassetspdlcom.cdnstatics2.com/usuaris/editorial/logo/d8253153-6647-454f-884e-b923429307f3-planeta.svg")
     private String image;
 }
