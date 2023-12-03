f9ae1235b29259d9a8474a8dbe47f0ca1b8a5933
Alexandru <alexandru.dorobat@educa.madrid.org>
2023-12-03 23:32:48 +0100

    fix(modell):documentamos nuesto modelo shop

--- src/main/java/com/nullers/restbookstore/rest/shop/model/Shop.java
+++ src/main/java/com/nullers/restbookstore/rest/shop/model/Shop.java
@@ -6,6 +6,7 @@ import com.nullers.restbookstore.rest.book.model.Book;
 import com.nullers.restbookstore.rest.client.model.Client;
 import com.nullers.restbookstore.rest.common.Address;
 import com.nullers.restbookstore.rest.orders.models.Order;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
@@ -38,32 +39,37 @@ public class Shop {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
+    @Schema(description = "ID", example = "1")
     private UUID id;
 
     @NotBlank(message = "El nombre no puede estar vacío")
+    @Schema(description = "Nombre", example = "Tienda Nullers")
     private String name;
 
     @Embedded
     @NotNull(message = "La ubicación no puede estar vacía")
+    @Schema(description = "Dirección de la tienda")
     @Valid
     private Address location;
 
     @CreatedDate
+    @Schema(description = "Fecha de creación", example = "2021-08-01T00:00:00.000Z")
     private LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Schema(description = "Fecha de actualización", example = "2021-08-01T00:00:00.000Z")
     private LocalDateTime updatedAt;
 
     @OneToMany
     @JoinColumn(name = "book_id")
-    @NotNull(message = "La tienda debe tener al menos un libro")
+    @Schema(description = "Conjunto de libros disponibles en la tienda")
     @Builder.Default()
     private Set<Book> books = Set.of();
 
 
     @OneToMany
     @JoinColumn(name = "client_id")
-    @NotNull(message = "La tienda debe tener al menos un cliente")
+    @Schema(description = "Conjunto de clientes de la tienda")    @NotNull(message = "La tienda debe tener al menos un cliente")
     @Builder.Default()
     private Set<Client> clients = Set.of();
 
